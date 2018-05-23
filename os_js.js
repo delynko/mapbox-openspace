3f3afe9ea8ecf2c35651bc4b34e6bb4db124d4a5
delynko <deynko@gmail.com>
2018-05-23 10:18:18 -0600

    change initial zoom

--- os_js.js
+++ os_js.js
@@ -3,7 +3,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/delynko/cjh10hlxk045o2rn0vzayaxq9',
     center: [-105.27829964111288, 39.82865670291332],
-    zoom: 12.5
+    zoom: 12.83
 });
 
 map.addControl(new mapboxgl.GeolocateControl({
